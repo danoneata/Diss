be68f560ed169c2006de6b09b3206bf838482591
Dan Oneata <dan.oneata@gmail.com>
2011-06-13 17:49:48 +0100

    Minor modifications: eliminated some useless lines.

--- Code/Scripts/try_nca.m
+++ Code/Scripts/try_nca.m
@@ -1,33 +1,22 @@
 clear all; close all; clc;
 
-% [X,c] = load_synthetic_data(150,7,'circles');
-
-% load('../Datasets/wine.mat');
-% load('../Datasets/transfusion.mat');
-% load('../Datasets/pima.mat');
-% [X c] = load_usps;
-
-% break;
-
+[X, c] = load_data_set('wine');
 % Subsample training set:
 % X = X(:,1:10:end);
 % c = c(:,1:10:end);
 
-[X, c] = load_data_set('wine');
 [X] = normalize_data(X);
-
 [D N] = size(X);
-% Plot initial data:
-plot3_data(X, [],c);
 
-fn = @(A)nca_obj(A, X, c);
+% Plot initial data:
+plot3_data(X, c);
 
 d = 2;
-A = minimize(randn(D*d,1), fn, 20);
+A = minimize(randn(D*d,1), 'nca_obj', 300, X, c);
 A = reshape(A, [], D);
 
-X2 = A*X;
-plot3_data(X2, [],c);
+AX = A*X;
+plot3_data(AX,c);
 
 % Construct k-d tree:
 depth = floor(log2(N));
@@ -40,4 +29,4 @@ global kdtree;
 kdtree(nr_nodes).point = [];
 kdtree(nr_nodes).split_dir = [];
 
-build_kdtree(X2, 1);
+build_kdtree(AX, 1);
