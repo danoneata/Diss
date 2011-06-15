57c2cfc211da798d24b53d62e0d92d5e0c16128e
Dan Oneata <dan.oneata@gmail.com>
2011-06-15 13:04:58 +0100

    Tried different stopping criteria. To be modified to incorporate mini-batch learning.

--- Code/Utilities/gd.m
+++ Code/Utilities/gd.m
@@ -1,32 +1,57 @@
-function [X, f_new, i] = gd(X0, f)
+function [X_new, f_new, i] = gd(X_old, f)
+%GD Basic implementation of gradient descent algorithm.
+%
+%     [X_new, f_new, i] = gd(X_old, f)
+%
+% Inputs:
+%      X_old Dx1 Initial point.
+%          f function handle.
+%
+% Outputs:
+%      X_new Dx1 solution.
+%      f_new 1x1 value of the function of the obtained solution X.
+%          i 1x1 number of iterations.
 
-  MAX_NR_ITER = 1e5;
+% Dan Oneata, June 2011
+% This function was inspired by the notes for the course CSC2515 
+% (University of Toronto).
 
-  step = median(abs(X0))/100;
+% TO DO: 
+%   * Add momentum?
+%   * Try other stopping criteria?
+
+  MAX_NR_ITER = 1000;
+
+  step = median(abs(X_old))/100;
+%   step = 5;
+  X_new = X_old + 1;
   f_old = Inf;
-  df_new = zeros(size(X0));
-  df_old = zeros(size(X0));
+  df_new = zeros(size(X_old));
+  df_old = zeros(size(X_old));
   i = 0;
 
-  while step > 1e-5,
-    X = X0 - step*df_new;
-    [f_new df_new] = feval(f, X);
-    if f_new >= f_old,
-      step = 0.5*step;
-      df_new = df_old;
-    else
-      step = 1.1*step;
-      f_old = f_new;
-      X0 = X;
-      df_old = df_new;
-    end
+  while step > 1e-5,          % What stopping condition should I use?
+%   while 1,
+    X_new = X_old - step*df_new;
+    [f_new df_new] = feval(f, X_new);
     
     i = i + 1;
-    if ~mod(i,100),
-      fprintf('Iteration %6i;  Value %4.6e\r', i, f_new);
+%     if (i > 0 && sum(abs(X_old - X_new)) < 1e-5) || i > MAX_NR_ITER, 
+%       break;
+%     end
+    
+    if f_new >= f_old,        % Worse.
+      step = 0.5*step;        % Slow down.
+      df_new = df_old;        % Use gradient from the previous step.
+    else                      % Better.
+      step = 1.1*step;        % Go faster.
+      f_old = f_new;          % Update function value;
+      X_old = X_new;          % Update position.
+      df_old = df_new;        % Update gradient.
     end
-    if i > MAX_NR_ITER,
-      break;
+    
+    if ~mod(i,10),
+      fprintf('Iteration %6i;  Value %4.6e\r', i, f_new);
     end
   end
   
