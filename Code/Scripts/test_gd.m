47f98b4d2cf28acbab5796c120d9edff1198aa29
Dan Oneata <dan.oneata@gmail.com>
2011-06-18 22:01:53 +0100

    Separated mini-batch iteration to sgd.m. Simplified the whole function.

--- Code/Scripts/test_gd.m
+++ Code/Scripts/test_gd.m
@@ -1,32 +1,19 @@
 clear all; close all;
 
-d = 2;
-MAX_NR_ITER = 10;
-BATCH_SIZE = 30;
-
-[X, c] = load_data_set('iris');
+[X, c] = load_data_set('landsat-train');
+X = double(X);
 X = normalize_data(X);
 [D, N] = size(X);
+
+d = 2;
 A = randn(D*d, 1);
 
-iter = 0;
-
-while iter < MAX_NR_ITER,
-  
-  iter = iter + 1;
-  idxs = randperm(N);
-  
-  iter
-  
-  for i = 1:BATCH_SIZE:N,
-    batch = X(:,idxs(i:min(N,i+BATCH_SIZE)));
-    batch_labels = c(:,idxs(i:min(N,i+BATCH_SIZE)));
-    
-    fn = @(A)nca_obj_reg(A, batch, batch_labels, 1e-2);
-    A = gd(A(:), fn);
-  end
-  
-end
+[A,f_all,step_all] = sgd(A, 'nca_obj', X, c);
+loocv(reshape(A,d,D)*X,c)
+% fn = @(A)nca_obj(A,X,c);
+% A = minimize(A,fn,300);
 
 Y = reshape(A, [d D])*X;
 plot3_data(Y, c);
+figure, plot(f_all);
+figure, plot(step_all);
