4f5cc23f255982dc6dab83d72d96b0e89fe27b57
s1053297 <s1053297@sms.ed.ac.uk>
2011-06-20 12:12:12 +0100

    Minor change: replaced tilde.

--- Code/NCA/nca_classification.m
+++ Code/NCA/nca_classification.m
@@ -1,13 +1,19 @@
 function [score, predictions] = nca_classification(X, c, Q, l)
 
-  [~, M] = size(Q);
+  M = size(Q,2);
 
   dd = exp(-square_dist(X, Q));
   
   fn = @sum;
   [rowidx, colidx] = ndgrid(c, 1:M);
   Mu = accumarray([rowidx(:) colidx(:)], dd(:), [], fn);
-  [~, idxs_min] = max(Mu, [], 1);
+  
+  % Scale everything at the number of elements in a class:
+  [dummy, idxs_class] = unique(c);
+  nr_elem_class = diff([0 idxs_class]);
+  
+  Mu_scaled = bsxfun(@times, Mu, 1./nr_elem_class');
+  [dummy, idxs_min] = max(Mu_scaled, [], 1);
   
   score = 1 - nnz(idxs_min - l)/M;
   predictions = idxs_min;
