60bc05ad607a861159de17efff0143223d6e38af
s1053297 <s1053297@sms.ed.ac.uk>
2011-06-21 14:03:34 +0100

    Added extra option to treat a specified objective function.

--- Code/Utilities/run_nca.m
+++ Code/Utilities/run_nca.m
@@ -1,12 +1,13 @@
-function [AX, mapping, score] = run_nca(X, c, d, opts)
+function [AX, mapping, score] = run_nca(X, c, d, obj, opts)
 %RUN_NCA Perform Neighbourhood Component Analysis.
 %
-%     [AX, mapping] = run_nca(X, c, d, opts)
+%     [AX, mapping] = run_nca(X, c, d, obj, opts)
 %
 % Inputs:
 %            X DxN data.
 %            c 1xN labels.
 %            d 1x1 number of dimensions to reduce to.
+%          obj string that specifies the NCA objective function to be used.
 %         opts 1x2 opt(1) defines initialization procedure: 
 %                   0 randn (default)
 %                   1 PCA 
@@ -25,6 +26,9 @@ function [AX, mapping, score] = run_nca(X, c, d, opts)
 
   if ~exist('d','var') || isempty(d),
     d = size(X,1);
+  end
+  if ~exist('obj','var') || isempty(obj),
+    obj = 'nca_obj';
   end
   if ~exist('opts','var') || isempty(opts),
     opts = zeros(1,2);
@@ -51,7 +55,7 @@ function [AX, mapping, score] = run_nca(X, c, d, opts)
       A = randn(d,D);
   end
   
-  iter = 0;
+  iter = 0; score = 0;
   converged = false;
   
   switch opts(2)
@@ -65,17 +69,19 @@ function [AX, mapping, score] = run_nca(X, c, d, opts)
         for batch = 1:batch_size:N
           X_batch = X( :, idxs( batch : min(batch + batch_size - 1, N) ) );
           c_batch = c( idxs( batch : min(batch + batch_size - 1, N) ) );
-          [A, f] = minimize(A(:), 'nca_obj', 5, X_batch, c_batch);
+          [A, f] = minimize(A(:), obj, 5, X_batch, c_batch);
 
           if isempty(f) || f(end) - f(1) > -1e-4
               disp('Converged!');
               converged = true;
+              score = -f(end)/size(X_batch,2);
           end
         end
       end
     otherwise
       % Train on the whole data at once:
-      [A f] = minimize(A(:), 'nca_obj', max_iter, X, c);
+      [A f] = minimize(A(:), obj, max_iter, X, c);
+      score = -f(end)/N;
   end
   
   A = reshape(A, [d D]);
@@ -83,7 +89,5 @@ function [AX, mapping, score] = run_nca(X, c, d, opts)
   mapping.A    = A * t.std;
   
   AX = A*X;
-  
-  score = -f(end)/N;
   
 end
