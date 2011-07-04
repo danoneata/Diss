e0a5272064d37726605e37258ee90ba18fceb6b9
s1053297 <s1053297@sms.ed.ac.uk>
2011-07-04 14:43:15 +0100

    Minor modifications in the function that determines the optimal mixing proportions.

--- Code/Scripts/test_cs_back_mixing.m
+++ Code/Scripts/test_cs_back_mixing.m
@@ -1,4 +1,4 @@
-function test_cs_back_mixing(dataset, mix_prop)
+function test_cs_back_mixing(dataset)
 %TEST_CS_BACK_MIXING Function to determine what mixing proportion is the 
 %most suitable for the NCA with compact support kernels and background
 %distribution.
@@ -12,49 +12,56 @@ function test_cs_back_mixing(dataset, mix_prop)
 
 % Dan Oneata, July 2011
 
-  root = '';
-  filename = [dataset '-' num2str(mix_prop) '.txt'];
-  T = 1;
+  root = '~/Documents/Diss/Results/mixing-proportion/';
+
+  T = 20;
   % Load data:
   [X,c] = load_data_set(dataset);
   [D] = size(X,1);
   dd = [2 3 D];
+  mix_props = [0.8 0.85 0.9 0.95 0.99 0.999];
   
   for idd = 1:length(dd);
-    d = dd(idd);
-    nca_score = zeros(1,T);
-    nn_score  = zeros(1,T);
-    for tt = 1:T,
-      % Split data into training set and testing set:
-      [X_train, c_train, X_test, c_test] = split_data(X,c);
-      [X_train, transf] = normalize_data(X_train);
-      % Get mean and covariance for each class:
-      moments = get_moments(X_train,c_train);
-      % Get number of points in each class:
-      [~,bb] = unique(c);
-      Nc = diff([0 bb]);
+    disp(dataset);
+    disp(dd(idd));
+    for mm = 1:length(mix_props);
+      mix_prop = mix_props(mm);
+      d = dd(idd);
+      nca_score = zeros(1,T);
+      nn_score  = zeros(1,T);
+      for tt = 1:T,
+        % Split data into training set and testing set:
+        [X_train, c_train, X_test, c_test] = split_data(X,c);
+        [X_train, transf] = normalize_data(X_train);
+        % Get mean and covariance for each class:
+        moments = get_moments(X_train,c_train);
+        % Get number of points in each class:
+        [dummy,bb] = unique(c_train);
+        Nc = diff([0 bb]);
 
-      % Initialize projection matrix:
-      A = init_A(X_train, c_train, moments, Nc, d, T);
-      % Minimize objective function:
-      A = minimize(A(:),'nca_obj_cs_back', 500, X_train, c_train, Nc, moments, mix_prop);
-      AA = reshape(A,d,D);
-      
-      transf.A = transf.std;
-      X_test = transform(X_test, transf);
-      % Get scores on test set:
-      nca_score(tt) = nca_cls_cs_back(X_train, c_train, X_test, c_test, AA, moments, Nc);
-      nn_score(tt) = NN_score(X_train, c_train, X_test, c_test, AA);
+        % Initialize projection matrix:
+        A = init_A(X_train, c_train, moments, Nc, d, T);
+        % Minimize objective function:
+        A = minimize(A(:),'nca_obj_cs_back', 500, X_train, c_train, Nc, moments, mix_prop);
+        AA = reshape(A,d,D);
+
+        transf.A = transf.std;
+        X_test = transform(X_test, transf);
+        % Get scores on test set:
+        nca_score(tt) = nca_cls_cs_back(X_train, c_train, X_test, c_test, AA, moments, Nc, mix_prop);
+        nn_score(tt) = NN_score(X_train, c_train, X_test, c_test, AA);
+      end
+      mean_nca_score = mean(nca_score);
+      std_nca_score = std(nca_score);
+
+      mean_nn_score = mean(nn_score);
+      std_nn_score = std(nn_score);
+
+      M = [mix_prop mean_nca_score std_nca_score mean_nn_score std_nn_score];
+      % Save results:
+      filename = [dataset '-' num2str(d) '.txt'];
+      dlmwrite([root filename], M, '-append', 'delimiter', '&');
     end
-    mean_nca_score = mean(nca_score);
-    std_nca_score = std(nca_score);
-    
-    mean_nn_score = mean(nn_score);
-    std_nn_score = std(nn_score);
-    
-    M = [d mean_nca_score std_nca_score mean_nn_score std_nn_score];
-    % Save results:
-    dlmwrite([root filename], M, '-append', 'delimiter', '&');
   end
 
 end
