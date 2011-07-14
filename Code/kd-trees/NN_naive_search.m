26fb4198979d24eb32da7d34911a24abf610ccf5
Dan Oneata <dan.oneata@gmail.com>
2011-07-14 12:29:57 +0100

    Modified function to return also the NN not only its index and the distance.

--- Code/kd-trees/NN_naive_search.m
+++ Code/kd-trees/NN_naive_search.m
@@ -1,9 +1,10 @@
-function [d_min idx_min] = NN_naive_search(X, q)
+function [d_min Xmin idx_min] = NN_naive_search(X, q)
 
   [D N] = size(X);
   Q = repmat(q,1,N);
   
   d = sum((X - Q).^2, 1);
   [d_min idx_min] = min(d);
+  Xmin = X(:,idx_min);
 
 end
