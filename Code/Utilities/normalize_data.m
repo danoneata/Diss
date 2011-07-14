6c3e194250ced8399d1d02bc2f3c8daca7e05ab4
Dan Oneata <dan.oneata@gmail.com>
2011-07-14 12:28:24 +0100

    Modified normalize data to ignore dimensions with no variation.

--- Code/Utilities/normalize_data.m
+++ Code/Utilities/normalize_data.m
@@ -1,11 +1,12 @@
 function [Y,transf] = normalize_data(X)
 
+  sd = std(X,0,2);
+  sd(sd == 0) = 1;
+  
   transf.mean = mean(X,2);
-  transf.std  = diag(1./std(X,0,2));
+  transf.std  = diag(1./sd);
 
   Y = bsxfun(@minus, X, mean(X,2));
-  Y = diag(1./std(X,0,2))*Y;  
+  Y = diag(1./sd)*Y;  
   
-%   Y = max(Y,1e-6);
-
 end
