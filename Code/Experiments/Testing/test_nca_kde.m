898aac9bdf2795775088a5bb067341c3a95dbec4
Dan Oneata <dan.oneata@gmail.com>
2011-07-18 16:08:19 +0100

    Minor changes of parameters.

--- Code/Experiments/Testing/test_nca_kde.m
+++ Code/Experiments/Testing/test_nca_kde.m
@@ -1,13 +1,15 @@
 %% Load data:
 clear all; close all;
 
-d = 3;
-[X,c] = load_data_set('iris');
+d = 2;
+idxs = [1 4 5 6 11 150];
+[X,c] = load_data_set('wine');
 X = normalize_data(X);
 [D,N] = size(X);
 
-A = randn(d,D);
+A = randn(d*D,1);
+% A = eye(d); A = A(:);
 
 %% Apply NCA:
-tic; [f, df] = nca_kde(A,X,c); toc;
-tic; [f2,df2] = nca_obj_simple(A,X,c); toc;
+tic; [f, df] = nca_kde(A,X,c,idxs); toc;
+tic; [f2,df2] = nca_obj_o1(A,X,c,idxs); toc;
