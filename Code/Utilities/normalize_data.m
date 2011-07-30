d0d93550162baea2ab6e187112f6ede604ed33b9
Dan Oneata <dan.oneata@gmail.com>
2011-07-30 16:01:15 +0100

    Minor changes to increase comaptibility. Introduced a sw to permit only data centering without normalization.

--- Code/Utilities/normalize_data.m
+++ Code/Utilities/normalize_data.m
@@ -1,12 +1,21 @@
-function [Y,transf] = normalize_data(X)
+function [Y,transf] = normalize_data(X, sw)
 
-  sd = std(X,0,2);
-  sd(sd == 0) = 1;
-  
-  transf.mean = mean(X,2);
-  transf.std  = diag(1./sd);
+  if ~exist('sw','var'),
+    sw = 0;
+  end
 
-  Y = bsxfun(@minus, X, mean(X,2));
-  Y = diag(1./sd)*Y;  
-  
+  if sw == 1,
+    transf.mean = mean(X,2);
+    transf.std  = eye(size(X,1));
+    Y = bsxfun(@minus, X, mean(X,2));
+  else
+    sd = std(X,0,2);
+    sd(sd == 0) = 1;
+
+    transf.mean = mean(X,2);
+    transf.std  = diag(1./sd);
+
+    Y = bsxfun(@minus, X, mean(X,2));
+    Y = diag(1./sd)*Y;  
+  end
 end
